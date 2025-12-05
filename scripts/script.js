10d9c50a7d43a8c5b857be05b93f64a07f1c53e9
Solomonwole <solomonwolemail@gmail.com>
2025-12-04 22:25:06 -0500

    display sections depending on department, and set default opened section based on department

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,6 @@ const menuIcon = document.getElementById("menu-icon");
 const popup = document.getElementById("notificationPopup");
 popup.classList.add("hidden");
 menuIcon.className = "bi bi-list";
-document.getElementById("rd2").checked = true;
 
 function toggleMenu() {
 	const navLinks = document.querySelector(".navbar-links");
